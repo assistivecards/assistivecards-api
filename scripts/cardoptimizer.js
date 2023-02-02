ef41f57152106cfab6f492c3415207bde9e65042
Burak Tokak <iletisim@buraktokak.com>
2023-02-03 01:12:50 +0300

    sync shape assets

--- scripts/cardoptimizer.js
+++ scripts/cardoptimizer.js
@@ -3,7 +3,7 @@ const compress_images = require("compress-images");
 let INPUT_path_to_your_images = "data/raw/**/*.{jpg,JPG,jpeg,JPEG,png,SVG,svg}";
 let OUTPUT_path = "data/cards/";
 
-compress_images(INPUT_path_to_your_images, OUTPUT_path, { compress_force: false, statistic: true, autoupdate: true }, false,
+compress_images(INPUT_path_to_your_images, OUTPUT_path, { compress_force: true, statistic: true, autoupdate: true }, false,
                 { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
                 { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
                 { svg: { engine: "svgo", command: "--multipass" } },
