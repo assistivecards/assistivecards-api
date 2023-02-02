8c3e90a56b7bb6560d5ee7ca4974f742dfecdc6c
Burak Tokak <iletisim@buraktokak.com>
2023-02-02 21:35:20 +0300

    better optimiser

--- scripts/cardoptimizer.js
+++ scripts/cardoptimizer.js
@@ -1,7 +1,7 @@
 const compress_images = require("compress-images");
 
-let INPUT_path_to_your_images = "data/cards/**/*.{jpg,JPG,jpeg,JPEG,png}";
-let OUTPUT_path = "data/cards_opt/";
+let INPUT_path_to_your_images = "data/raw/**/*.{jpg,JPG,jpeg,JPEG,png,SVG,svg}";
+let OUTPUT_path = "data/cards/";
 
 compress_images(INPUT_path_to_your_images, OUTPUT_path, { compress_force: false, statistic: true, autoupdate: true }, false,
                 { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
